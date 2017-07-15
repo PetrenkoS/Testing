f943d8378b0bfc0f4f1904e9fbab9be959adf979
PetrenkoS <petrenko.s@yahoo.com>
2017-07-15 10:08:09 +0300

    Тесты работают в IE и Chrome

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -3,10 +3,8 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
-import static org.openqa.selenium.remote.BrowserType.*;
 
 import java.util.concurrent.TimeUnit;
 
@@ -31,7 +29,7 @@ public class ApplicationManager {
 
   public void init() {
     if (browser == BrowserType.FIREFOX) {
-      wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+      wd = new FirefoxDriver();
     } else if (browser == BrowserType.CHROME) {
       wd = new ChromeDriver();
     } else if (browser == BrowserType.IE) {
