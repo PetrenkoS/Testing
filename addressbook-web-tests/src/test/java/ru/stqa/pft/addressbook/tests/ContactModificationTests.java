fde13db5ec5001b423c8ac7fa1889ad500a7476d
PetrenkoS <petrenko.s@yahoo.com>
2017-07-16 15:19:22 +0300

    Завершена сортировка списка контактов по  лекции 4.10

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -21,18 +22,21 @@ public class ContactModificationTests extends TestBase {
     }
     app.getNavigationHelper().gotoHomePage();
     List<ContactData> before = app.getContactHelper().getContactList();
-    app.getContactHelper().selectContact(before.size() - 2);
+    app.getContactHelper().selectContact(0);
     app.getContactHelper().initContactModification();
-    ContactData contact = new ContactData(before.get(before.size() - 2).getId(),"Donald", "Trump", "bxlvz", "gsi", null);
+    ContactData contact = new ContactData(before.get(0).getId(),"Donald", "Trump", "bxlvz", "gsi", null);
     app.getContactHelper().fillContactForms(contact, false);
     app.getContactHelper().submitContactModification();
     app.getNavigationHelper().gotoHomePage();
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size());
 
-    before.remove(before.size() - 2);
+    before.remove(0);
     before.add(contact);
-    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+    Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
   }
 
 }
