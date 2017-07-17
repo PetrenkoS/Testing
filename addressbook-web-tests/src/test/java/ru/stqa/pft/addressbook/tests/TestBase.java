5102fbeccd71167bfb4c393c20719a44e7d216b5
PetrenkoS <petrenko.s@yahoo.com>
2017-07-17 12:59:26 +0300

    Лекция 5.1. Тесты вместе все равно не запускаются.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,9 +1,11 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 /**
  * Created by Admin on 12.07.2017.
@@ -11,14 +13,14 @@ import org.openqa.selenium.remote.BrowserType;
 public class TestBase {
 
 
-  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
-  @BeforeMethod
+  @BeforeSuite
   public void setUp() throws Exception {
     app.init();
   }
 
-  @AfterMethod
+  @AfterSuite
   public void tearDown() {
     app.stop();
   }
