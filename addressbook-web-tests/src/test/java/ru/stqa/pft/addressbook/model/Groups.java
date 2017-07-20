73b291c63bdf4492ca5f297b9d07658e655ed93e
PetrenkoS <petrenko.s@yahoo.com>
2017-07-20 10:43:42 +0300

    Внесены изменения в Groups, чтобы ДЗ 10 заработало

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/Groups.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/Groups.java
@@ -21,9 +21,11 @@ public class Groups extends ForwardingSet<GroupData> {
   }
 
 
+
   @Override
+
   protected Set<GroupData> delegate() {
-    return null;
+        return delegate;
   }
 
   public Groups withAdded(GroupData group) {
