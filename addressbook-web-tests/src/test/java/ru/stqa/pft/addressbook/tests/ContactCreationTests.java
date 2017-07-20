77ef4008c733ed10bd71bd5d90bbc748a51a9714
PetrenkoS <petrenko.s@yahoo.com>
2017-07-20 17:25:26 +0300

    Лекция 6.4. для контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,34 +1,40 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.junit.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test
-  public void testsContactCreation() {
+  @DataProvider
+  public Iterator<Object[]> validContacts() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new ContactData().withFirstname("firstname1").withLastname("lastname1").withAddress("address1").withHometelephone("hometelephone1")});
+    list.add(new Object[] {new ContactData().withFirstname("firstname2").withLastname("lastname2").withAddress("address2").withHometelephone("hometelephone2")});
+    list.add(new Object[] {new ContactData().withFirstname("firstname3").withLastname("lastname3").withAddress("address3").withHometelephone("hometelephone3")});
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validContacts")
+  public void testsContactCreation(ContactData contact) {
     app.goTo().homePage();
     Contacts before = app.contact().all();
     app.goTo().gotoAddContactPage();
-    ContactData contact = new ContactData()
-            .withFirstname("Vladimir").withLastname("Putin").withAddress("Kremlin").withHometelephone("111").withMobilePhone("756").withWorkPhone("7913")
-            .withEmail("hfbsk@").withEmail2("bhskd@").withEmail3("hbls@").withPhoto(null);
-    File photo = new File("src/test/resources/stru.jpeg");
-    app.contact().create(new ContactData()
-            .withFirstname("Putin").withLastname("Vladimir").withAddress("Kremlin").withHometelephone("111").withMobilePhone("756").withWorkPhone("7913")
-            .withEmail("hfbsk@").withEmail2("bhskd@").withEmail3("hbls@").withPhoto(photo), true); // Эта строчка не работает почему-то, если поставить "contact, true"
+    app.contact().create(contact, true);
     app.goTo().homePage();
     Contacts after = app.contact().all();
     assertThat(after.size(), equalTo(before.size() + 1));
 
-    //assertThat(after, equalTo(
-           // before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+
   }
 
   }
