6070015e79be82f5a13699bb33eb6d48991d84a0
PetrenkoS <petrenko.s@yahoo.com>
2017-07-24 17:29:46 +0300

    Реализованы отключаемые проверки UI на контактах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -49,6 +49,7 @@ public class ContactCreationTests extends TestBase {
     assertThat(after.size(), equalTo(before.size() + 1));
 //    assertThat(after, equalTo(before.withAdded(
            // contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+    verifyContactListInUI();
 
 
   }
